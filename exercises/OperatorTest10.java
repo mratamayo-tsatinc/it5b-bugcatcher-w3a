7d1b3e070845665d0e31a348980ed4fa0581670b
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-07-29 14:16:24 +0800

    Update OperatorTest10.java

--- exercises/OperatorTest10.java
+++ exercises/OperatorTest10.java
@@ -1,6 +1,6 @@
 public class OperatorTest10 {
     public static void main(String[] args) {
         int num = 15 + 3;
-        System.out.println(num);
-    }[[;]]
+        System[[;]]out.println(num);
+    }
 }
