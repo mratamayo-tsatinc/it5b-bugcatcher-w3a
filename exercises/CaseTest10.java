40ceb499790dfe5f26acb51c8862bd7bfdc8c2f5
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-07-29 14:03:01 +0800

    Update and rename CaseTest10.java to OperatorTest2.java

--- exercises/CaseTest10.java
+++
@@ -1,6 +0,0 @@
-public class CaseTest10 {
-    public static void main(String[] args) {
-        [[Double]] price = 19.99; // Intended primitive type
-        System.out.println(price);
-    }
-}
---
+++ exercises/OperatorTest2.java
@@ -0,0 +1,6 @@
+public class OperatorTest2 {
+    public static void main(String[] args) {
+        int result = 10[[:]]
+        System.out.println(result);
+    }
+}
