ff48270c498b52e83079b8fd0b1b84b6527ea783
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-07-29 14:07:29 +0800

    Update and rename CaseTest7.java to OperatorTest8.java

--- exercises/CaseTest7.java
+++
@@ -1,5 +0,0 @@
-public class CaseTest7 {
-    public static void main(String[] args) {
-        System.[[Out]].println("Output message");
-    }
-}
---
+++ exercises/OperatorTest8.java
@@ -0,0 +1,9 @@
+import java.util[[;]]Scanner;
+
+public class OperatorTest8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double val = sc.nextDouble();
+        System.out.println(val);
+    }
+}
