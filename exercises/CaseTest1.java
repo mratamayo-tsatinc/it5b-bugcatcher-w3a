d1bae9c9dfe89103cb51dfc0ff35ae3df99fe950
Mark Robert <143669058+mratamayo-tsatinc@users.noreply.github.com>
2026-07-29 14:02:16 +0800

    Update and rename CaseTest1.java to OperatorTest1.java

--- exercises/CaseTest1.java
+++
@@ -1,5 +0,0 @@
-[[Public]] class CaseTest1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ exercises/OperatorTest1.java
@@ -0,0 +1,6 @@
+public class OperatorTest1[[;]] {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println(age);
+    }
+}
